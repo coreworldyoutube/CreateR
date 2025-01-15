7e878d2e765de7ffd627cc32a24cca1e57357957
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2025-01-15 10:56:26 +0900

    Update CreateR.java

--- src/main/coreworldy/CreateR/CreateR.java
+++ src/main/coreworldy/CreateR/CreateR.java
@@ -1,1 +1,19 @@
-a
+package coreworldy.CreateR;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraft.client.Minecraft;
+
+@Mod("creater")
+public class CreateR
+{
+    // MODのID
+    public static final String MODID = "creater"; 
+}
